cd77d90f0fc438054748d65ee84c337a20b52bdb
Наумочкина Евгения <279015580@TOP.ACADEMY>
2024-11-25 21:00:06 +0300

    Добавлен поиск по фильмам

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-
-import { useState } from 'react'
+import { APIKEY } from './env';
+import { useState, useEffect } from 'react'
+import Movies from './components/Movies/Movies'
 
 function App() {
+  let [searchField, setSearchField] = useState("")
+  const URL = `https://api.kinopoisk.dev/v1.4/movie/search?query=` 
+
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const res = await fetch(URL+ searchField, {
+        headers: {
+          'X-API-KEY': APIKEY
+        }
+      })  
+      const data = await res.json()
+      setMovies(data.docs)
+    } 
+
+    fetchMovies()
+  }, [URL, searchField])
+
+  let timer;
+
+  function handleChange(event) {
+    clearTimeout(timer)
+
+    timer = setTimeout(() => {
+      setSearchField(event.target.value)
+    }, 1000)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <input type="text" onChange={handleChange} />
+        <Movies movies={movies} />
       </header>
     </div>
   );
